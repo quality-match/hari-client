4796a7a5e23c923f1adb9e3eeb26509a1b9c7475
Schmaexxi <max.langknecht@quality-match.com>
2024-10-17 23:30:16 +0200

    Add conftest + upload fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+from hari_client import Config
+from hari_client import HARIClient
+
+pytest_plugins = [
+    "tests.upload.fixtures",
+]
+
+
+@pytest.fixture()
+def test_client() -> HARIClient:
+    test_config = Config(
+        hari_username="username",
+        hari_password="password",
+        hari_api_base_url="api_base_url",
+        hari_client_id="client_id",
+        hari_auth_url="auth_url",
+    )
+    return HARIClient(config=test_config)
