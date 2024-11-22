42ac371d18727adbf8fd6285ea3c905372a38dce
Lars Schmarje <lars.schmarje@quality-match.com>
2024-11-22 16:33:53 +0100

    Update location of HARI object, moved to main function

--- scripts/create_subsets_from_attribute.py
+++ scripts/create_subsets_from_attribute.py
@@ -1,27 +1,27 @@
 import argparse
 import json
-from typing import List, Optional
+from typing import List
 from uuid import UUID
 
-from hari_client import Config, HARIClient
+from hari_client import Config
+from hari_client import HARIClient
 from hari_client.models import models
-from hari_client.models.models import QueryParameter, QueryList, SubsetType
+from hari_client.models.models import SubsetType
 
 
-def create_subsets_for_attribute(dataset_id: str, attribute_id: str, prefix: str) -> None:
+def create_subsets_for_attribute(
+    hari: HARIClient, dataset_id: str, attribute_id: str, prefix: str
+) -> None:
     """
     Function to create subsets in a HARI dataset based on a given attribute.
     Disclaimer: This function only works for MediaObjects subsets. It uses the first visualization config by default.
 
     Args:
+        hari (HARIClient): API Client
         dataset_id (str): The ID of the dataset to work on.
         attribute_id (str): The ID of the attribute to query.
         prefix (str): A prefix to add to subset names for better sorting.
     """
-
-    # 1. Initialize the HARI client using the configuration from the .env file.
-    config: Config = Config(_env_file='.env')
-    hari: HARIClient = HARIClient(config=config)
 
     dataset_id = UUID(dataset_id)
 
@@ -31,11 +31,7 @@ def create_subsets_for_attribute(dataset_id: str, attribute_id: str, prefix: str
 
     # 2. Get attributes matching the provided attribute ID.
     # Construct a query to find the attribute with the specified ID.
-    query: dict = {
-        'attribute': 'id',
-        'query_operator': '==',
-        'value': attribute_id
-    }
+    query: dict = {"attribute": "id", "query_operator": "==", "value": attribute_id}
 
     # Execute the query to retrieve the attributes.
     attributes = hari.get_attributes(dataset_id, query=json.dumps(query))
@@ -43,16 +39,23 @@ def create_subsets_for_attribute(dataset_id: str, attribute_id: str, prefix: str
 
     # 3. Extract needed subsets.
     # Collect all unique labels from the attribute frequencies.
-    labels: List[str] = list(set([
-        label
-        for attribute in attributes if attribute.frequency is not None
-        for label in attribute.frequency.keys()
-    ]))
+    labels: List[str] = list(
+        set(
+            [
+                label
+                for attribute in attributes
+                if attribute.frequency is not None
+                for label in attribute.frequency.keys()
+            ]
+        )
+    )
     print(f"Need to create subsets for labels: {labels}")
 
     # 4. Create subsets.
     # Retrieve available visualization configurations for the dataset.
-    visconfigs: List[models.VisualisationConfiguration] = hari.get_vis_configs(dataset_id)
+    visconfigs: List[models.VisualisationConfiguration] = hari.get_vis_configs(
+        dataset_id
+    )
     if not visconfigs:
         print("No visualization configurations found for the dataset.")
         return
@@ -65,14 +68,14 @@ def create_subsets_for_attribute(dataset_id: str, attribute_id: str, prefix: str
     for label in labels:
         # Define the query to select media objects with the specific attribute and label.
         label_query: dict = {
-            'attribute': f'attributes.{attribute_id}.dataset_id',
-            'query_operator': '==',
-            'value': dataset_id
+            "attribute": f"attributes.{attribute_id}.dataset_id",
+            "query_operator": "==",
+            "value": dataset_id,
         }
         dataset_query: dict = {
-            'attribute': f'attributes.{attribute_id}.aggregate',
-            'query_operator': "in",
-            'value': [label]
+            "attribute": f"attributes.{attribute_id}.aggregate",
+            "query_operator": "in",
+            "value": [label],
         }
         subset_name: str = f"{prefix}{label}"
         print(f"Creating subset: {subset_name}")
@@ -83,37 +86,33 @@ def create_subsets_for_attribute(dataset_id: str, attribute_id: str, prefix: str
             subset_type=SubsetType.MEDIA_OBJECT,
             subset_name=subset_name,
             visualisation_config_id=vis_config_id,
-            filter_options=[label_query, dataset_query]
+            filter_options=[label_query, dataset_query],
         )
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Argument parser setup.
     parser = argparse.ArgumentParser(
-        description='Create subsets in a HARI dataset based on an attribute.'
+        description="Create subsets in a HARI dataset based on an attribute."
     )
 
     # Add command-line arguments.
     parser.add_argument(
-        '-a', '--attribute_id',
+        "-a",
+        "--attribute_id",
         type=str,
-        help='Attribute ID which should be transformed into ',
-        required=True
+        help="Attribute ID which should be transformed into ",
+        required=True,
     )
     parser.add_argument(
-        '-d', '--dataset_id',
-        type=str,
-        help='Dataset ID to work on.',
-        required=True
+        "-d", "--dataset_id", type=str, help="Dataset ID to work on.", required=True
     )
     parser.add_argument(
-        '-p', '--prefix',
+        "-p",
+        "--prefix",
         type=str,
-        help='Prefix to add to subset names for better sorting.',
-        default=""
+        help="Prefix to add to subset names for better sorting.",
+        default="",
     )
 
     # Parse the arguments.
@@ -124,5 +123,9 @@ if __name__ == '__main__':
     dataset_id: str = args.dataset_id
     prefix: str = args.prefix
 
+    # load hari client
+    config: Config = Config(_env_file=".env")
+    hari: HARIClient = HARIClient(config=config)
+
     # Call the main function.
-    create_subsets_for_attribute(dataset_id, attribute_id, prefix)
+    create_subsets_for_attribute(hari, dataset_id, attribute_id, prefix)
