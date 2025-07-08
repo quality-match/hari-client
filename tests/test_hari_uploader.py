56495d63f32031b1de7db72aa50850db5ced9cd0
Schmaexxi <max.langknecht@quality-match.com>
2025-07-08 10:15:44 +0200

    Test upload progress bars are computed correctly

--- tests/test_hari_uploader.py
+++ tests/test_hari_uploader.py
@@ -1888,3 +1888,12 @@ def test_hari_uploader_marks_dependencies_as_failed_when_media_object_upload_fai
         len(results.failures.failed_media_object_attributes)
         == uploader_result["num_failed_media_object_attributes"]
     )
+    assert uploader._media_upload_progress.total == uploader._media_upload_progress.n
+    assert (
+        uploader._media_object_upload_progress.total
+        == uploader._media_object_upload_progress.n
+    )
+    assert (
+        uploader._attribute_upload_progress.total
+        == uploader._attribute_upload_progress.n
+    )
